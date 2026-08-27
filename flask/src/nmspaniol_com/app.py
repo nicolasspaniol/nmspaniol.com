3a4fe725b2f085a2d8255b0b58e80ddd46bf1ebf
nmspaniol <nicolasmspaniol@gmail.com>
2026-08-27 00:43:21 -0300

    started working on the actual website

--- flask/src/nmspaniol_com/app.py
+++ flask/src/nmspaniol_com/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env uv run
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.get('/')
 def todo():
-    return "boa noite"
+    return render_template('base.html')
